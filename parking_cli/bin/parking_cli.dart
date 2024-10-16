d3d4e3fd26205f472eeb373cc3be6c133d7fbab7
Anna Nilsson-Niia <annanilssonniia@gmail.com>
2024-10-16 20:48:32 +0200

    Create parking cli

---
+++ parking_cli/bin/parking_cli.dart
@@ -0,0 +1,5 @@
+import 'package:parking_cli/parking_cli.dart' as parking_cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${parking_cli.calculate()}!');
+}
---
+++ parking_cli/test/parking_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:parking_cli/parking_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
